13f80cc601f8527845a7604cd1c95edf30ee0508
Dzuma <A1genius@mail.ru>
2022-12-07 20:06:13 +0500

    Сортировка пузырьком, шейкер , merge, Вставка

---
+++ Вставка.py
@@ -0,0 +1,11 @@
+def insertion_sort(list1):
+    # Outer loop to traverse through 1 to len(list1)
+    for i in range(1, len(list1)):
+        value = list1[i]
+        j = i - 1
+        while j >= 0 and value < list1[j]:
+            list1[j + 1] = list1[j]
+            j -= 1
+        list1[j + 1] = value
+    return list1
+
